a5b261f767266ae6b4e2f1b892f1548ade7d2779
Daniel Griffiths <logiconictvtube@gmail.com>
2017-02-07 08:03:59 -0500

    calculate the loudness of a sound wave, given its intensity as a parameter to the dBScale/db_scale function.

--- javascript_code_wars.js
+++ javascript_code_wars.js
@@ -8,18 +8,26 @@ function annulusArea(a) {
     return Number(p.toFixed(2));
   }
 }
-
+// 
 function annulusArea(a) {
   const r1 = a/Math.sqrt(3)/2
   const r2 = a/Math.sqrt(3)
 
   return +(Math.PI * r2 * r2 - Math.PI * r1 * r1).toFixed(2)
 }
-
+//
 function annulusArea(a) {
   return parseFloat((Math.PI*(a/2)*(a/2)).toFixed(2));
 }
-
+//
 function annulusArea(a) {
   return Math.round(Math.PI*a*a/2/2*100)/100
+}
+
+// 2     calculate the loudness of a sound wave, given its intensity as a parameter to the dBScale/db_scale function.
+
+const dBScale = intensity => 10 * (12 + Math.log10(intensity));
+//
+function dBScale(intensity) {
+  return 10*(Math.log10(intensity)+12);
 }
