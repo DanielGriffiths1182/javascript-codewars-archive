92f9163a8ba780234b88b8a2379a3a8de026ccd4
Daniel Griffiths <logiconictvtube@gmail.com>
2017-02-10 21:13:25 -0500

    binary equivilent of sum

--- javascript_code_wars.js
+++ javascript_code_wars.js
@@ -8,7 +8,7 @@ function annulusArea(a) {
     return Number(p.toFixed(2));
   }
 }
-// 
+//
 function annulusArea(a) {
   const r1 = a/Math.sqrt(3)/2
   const r2 = a/Math.sqrt(3)
@@ -30,4 +30,20 @@ const dBScale = intensity => 10 * (12 + Math.log10(intensity));
 //
 function dBScale(intensity) {
   return 10*(Math.log10(intensity)+12);
+}
+
+// 3    Given an array of elements, return the binary equivilent of the sum of the elements
+function arr2bin(arr){
+  return arr.reduce((x,y)=>x+(typeof y=="number"?y:0),0).toString(2);
+}
+
+
+function arr2bin(arr){
+  var sum = 0;
+  arr.forEach(function(num){
+    if(typeof num === 'number'){
+      sum += num;
+    }
+  });
+  return sum.toString(2);
 }
