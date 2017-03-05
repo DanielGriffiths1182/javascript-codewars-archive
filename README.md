6406c777c87a9d371a4c008580f98383aeba3876
Daniel Griffiths <logiconictvtube@gmail.com>
2017-03-05 08:16:55 -0500

    JavaScript-35-Plural-description-README--practice shorthand if/else--

--- README.md
+++ README.md
@@ -167,6 +167,9 @@
 
 - 34 (Needed assistance, didnt solve on my own) Invert a given list of integer values.
 
+- 35 We need a simple function that determines if a plural is needed or not. It should take a number, and return
+ true if a plural should be used with that number or false if not. This would be useful when printing out a string
+ such as 5 minutes, 14 apples, or 1 sun.
 
 
 
--- javascript_code_wars.js
+++ javascript_code_wars.js
@@ -429,3 +429,10 @@ function numberJoy(n) {
 function invert(array) {
    return array.map( x => x === 0 ? x : -x);
 }
+
+//  35 "Plural" - We need a simple function that determines if a plural is needed or not. 
+//     It should take a number, and return true if a plural should be used with that number or false if not.
+//     This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+function plural(n) {
+  return n === 1 ? false : true;
+}
